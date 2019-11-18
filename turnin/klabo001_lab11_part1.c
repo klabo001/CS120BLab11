076bf61a1e2da1a9a2102579f49e4266502081cf
klabo001 <38570199+klabo001@users.noreply.github.com>
2019-11-18 08:18:08 -0800

    Update klabo001_lab11_part1.c

--- turnin/klabo001_lab11_part1.c
+++ turnin/klabo001_lab11_part1.c
@@ -13,6 +13,33 @@
 #endif
 
 
+// Permission to copy is granted provided that this header remains intact.
+// This software is provided with no warranties.
+
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef BIT_H
+#define BIT_H
+
+////////////////////////////////////////////////////////////////////////////////
+//Functionality - Sets bit on a PORTx
+//Parameter: Takes in a uChar for a PORTx, the pin number and the binary value
+//Returns: The new value of the PORTx
+unsigned char SetBit(unsigned char pin, unsigned char number, unsigned char bin_value)
+{
+	return (bin_value ? pin | (0x01 << number) : pin & ~(0x01 << number));
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//Functionality - Gets bit from a PINx
+//Parameter: Takes in a uChar for a PINx and the pin number
+//Returns: The value of the PINx
+unsigned char GetBit(unsigned char port, unsigned char number)
+{
+	return ( port & (0x01 << number) );
+}
+
+#endif //BIT_H
 // Returns '\0' if no key pressed, else returns char '1', '2', ... '9', 'A', ... 
 // If multiple keys pressed, returns leftmost-topmost one
 // Keypad must be connected to port C
